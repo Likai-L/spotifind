263407cbe02869afcd8c34d8a2f80f3de2f09120
Justin <justinjdbennett@gmail.com>
2023-02-03 22:56:46 -0700

    ESLint

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,5 @@
-import '@/styles/globals.scss'
+import '@/styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
