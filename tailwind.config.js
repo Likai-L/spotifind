f4656f54c2a6b6c10e789604d8eb11a94f256bc2
Justin <justinjdbennett@gmail.com>
2023-02-05 01:45:26 -0700

    Set theme bg and font colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultConfig');
+
+const generateColorClass = variable => {
+  return `var(--${variable})`;
+};
+
+const textColor = {
+  primary: generateColorClass('text-primary'),
+  secondary: generateColorClass('text-secondary'),
+  nav: generateColorClass('text-nav')
+};
+
+const backgroundColor = {
+  primary: generateColorClass('bg-primary'),
+  secondary: generateColorClass('bg-secondary'),
+  main: generateColorClass('bg-main'),
+  nav: generateColorClass('bg-nav'),
+  button: generateColorClass('bg-button')
+};
+
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './src/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
+  important: true,
   theme: {
-    extend: {}
+    ...defaultTheme,
+    extend: {
+      textColor,
+      backgroundColor
+    }
   },
   plugins: []
 };
