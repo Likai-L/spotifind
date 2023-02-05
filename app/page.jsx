361c50f8d8674e9ead0b21af7c3d89750fb9a8c6
Justin <justinjdbennett@gmail.com>
2023-02-04 20:10:40 -0700

    Move to using Next 13 app dir

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className="text-red-500">I am Home</div>
+    </main>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
+// const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
+  // sassOptions: { // If using sass instead of postCSS
+  //   includePaths: [path.join(__dirname, 'styles')]
+  // },
+  experimental: {
+    appDir: true
   }
 };
 
