420d43450dea4edb2a30f601c1b1c560a9665707
larsyaeger <larsyaeger@gmail.com>
2023-02-06 10:40:51 -0600

    got a barebones chat to appear with hard coded values of my created user Larsy

--- app/messages/page.jsx
+++ app/messages/page.jsx
@@ -1,5 +1,14 @@
+'use client';
 import styles from '@/styles/Home.module.css';
+import { ChatEngine } from 'react-chat-engine';
 
 export default function Messages() {
-  return <div className={`text-green-500 ${styles.main}`}>Messages</div>;
+  return (
+    <div className={`text-green-500 ${styles.main}`}>Messages
+      <ChatEngine
+        projectID='f06a82ab-ee91-4d7d-9b6d-90b79d3392ca'
+        userName='Larsy'
+        userSecret='123qwe'
+      />
+    </div>);
 }
