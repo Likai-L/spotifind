c477e5533f5d7f2d19814173284d9170a743e286
Likai Liu <likailiu2@gmail.com>
2023-02-07 05:52:22 +0800

    Store auth code on the frontend nad call useAuth with code passed in

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,10 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import useAuth from '@/hooks/useAuth';
 export default function Home() {
-  const clientId = process.env.SPOTIFY_CLIENT_ID;
-  const redirectUri = process.env.REDIRECT_URI;
+  const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
+  const redirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI;
 
   const scope =
     'user-read-private user-read-email user-read-currently-playing user-top-read user-read-recently-played playlist-read-private';
@@ -8,11 +12,14 @@ export default function Home() {
   const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope
     .split(' ')
     .join('%20')}&response_type=code&show_dialog=true`;
-
+  const searchParams = useSearchParams();
+  const authCode = searchParams.get('code');
+  useAuth(authCode);
   return (
-    <div className="text-green-500">
+    <div className="text-white-500">
       I am Home (AKA Now Playing)
       <a href={authUrl}>Login with Spotify</a>
+      {authCode && <h2>{authCode}</h2>}
     </div>
   );
 }
