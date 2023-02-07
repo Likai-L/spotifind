98fdbff13230a235cf6ec62efe261e78c4cc8a7c
Likai Liu <likailiu2@gmail.com>
2023-02-07 10:19:33 +0800

    Check if code is provided, if not send a 400 error

--- src/pages/api/token.js
+++ src/pages/api/token.js
@@ -8,27 +8,29 @@ const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 export default async function handler(req, res) {
   const code = req.query.code || null;
   // const state = req.query.state || null;
-  if (code) {
-    const authOptions = {
-      body: {
-        code,
-        redirect_uri: redirectUri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          `${clientId}:${clientSecret}`
-        ).toString('base64')}`,
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    };
-    axios
-      .post('https://accounts.spotify.com/api/token', authOptions.body, {
-        headers: authOptions.headers
-      })
-      .then(response => {
-        res.status(200).json(response.data);
-      })
-      .catch(err => console.log(err.message));
+  if (!code) {
+    res.status(400).json({ error: 'No auth code provided' });
+    return;
   }
+  const authOptions = {
+    body: {
+      code,
+      redirect_uri: redirectUri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      Authorization: `Basic ${Buffer.from(
+        `${clientId}:${clientSecret}`
+      ).toString('base64')}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  };
+  axios
+    .post('https://accounts.spotify.com/api/token', authOptions.body, {
+      headers: authOptions.headers
+    })
+    .then(response => {
+      res.status(200).json(response.data);
+    })
+    .catch(err => console.log(err.message));
 }
