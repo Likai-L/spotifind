b2841100aaeca942a5941006111fe23cef3776d7
larsyaeger <larsyaeger@gmail.com>
2023-02-07 15:21:35 -0600

    deconstruct props

--- app/(context)/index.jsx
+++ app/(context)/index.jsx
@@ -1,12 +1,14 @@
-'use client'
-import React, { useState, createContext } from 'react';
-//This basically initializes an instance of a context API
+'use client';
+
+import { createContext, useContext, useState } from 'react';
+// This basically initializes an instance of a context API
 export const GlobalContext = createContext();
 
-export const GlobalContextProvider = props => {
-  //state will be accessible globally, should be easy enough to link to spotify, there's a guide on connecting to existing stuff
-  const [username, setUsername] = useState("");
-  const [secret, setSecret] = useState("");
+export function GlobalContextProvider(props) {
+  // state will be accessible globally, should be easy enough to link to spotify, there's a guide on connecting to existing stuff
+  const [username, setUsername] = useState('');
+  const [secret, setSecret] = useState('');
+  const { children } = props;
 
   const globalStates = {
     username,
@@ -14,9 +16,13 @@ export const GlobalContextProvider = props => {
     secret,
     setSecret
   };
-  //component provider, wraps around other components, components that are wrapped
-  //have access to the context username, secret, basically which account is logged in
-  //and if there is anyone logged in at all
-  return <GlobalContext.Provider value={globalStates}>{props.children}</GlobalContext.Provider>;
-};
+  // component provider, wraps around other components, components that are wrapped
+  // have access to the context username, secret, basically which account is logged in
+  // and if there is anyone logged in at all
+  return (
+    <GlobalContext.Provider value={globalStates}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
 export const useGlobalContext = () => useContext(GlobalContext);
