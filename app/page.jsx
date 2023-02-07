a46dd4bb5999eed0f25973d982c7f8f1deaeee94
Likai Liu <likailiu2@gmail.com>
2023-02-07 12:20:35 +0800

    Fetch access_token etc. from params and store them in states

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,25 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
 import Button from './(button)/Button';
 import SpotifyIcon from './(nav)/(icons)/SpotifyIcon';
 
 export default function Home() {
+  const [accessToken, setAccessToken] = useState();
+  const [refreshToken, setRefreshToken] = useState();
+  const [expiresIn, setExpiresIn] = useState();
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    // since three query strings come together just check for one
+    if (searchParams.get('access_token')) {
+      console.log('SETTING STATES');
+      setAccessToken(searchParams.get('access_token'));
+      setRefreshToken(searchParams.get('refresh_token'));
+      setExpiresIn(searchParams.get('expires_in'));
+    }
+  }, []);
+
   const loginButtonContent = () => {
     return (
       <span className="flex items-center">
