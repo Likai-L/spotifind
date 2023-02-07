af7eeb6ac2adb5efef5c2830a6e874c1010ba139
Likai Liu <likailiu2@gmail.com>
2023-02-07 10:14:37 +0800

    Move the authorize request back to the backend

--- app/page.jsx
+++ app/page.jsx
@@ -2,16 +2,6 @@ import Button from './(button)/Button';
 import SpotifyIcon from './(nav)/(icons)/SpotifyIcon';
 
 export default function Home() {
-  const clientId = process.env.SPOTIFY_CLIENT_ID;
-  const redirectUri = process.env.REDIRECT_URI;
-
-  const scope =
-    'user-read-private user-read-email user-read-currently-playing user-top-read user-read-recently-played playlist-read-private';
-
-  const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope
-    .split(' ')
-    .join('%20')}&response_type=code&show_dialog=true`;
-
   const loginButtonContent = () => {
     return (
       <span className="flex items-center">
@@ -26,7 +16,11 @@ export default function Home() {
       <br />
       <br />
       <div>
-        <Button content={loginButtonContent()} path={authUrl} target="_blank" />
+        <Button
+          content={loginButtonContent()}
+          path="/api/login"
+          target="_blank"
+        />
       </div>
     </div>
   );
