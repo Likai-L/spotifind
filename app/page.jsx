a4652333afa5a860380f4672ce330941b1612ee4
Likai Liu <likailiu2@gmail.com>
2023-02-09 06:32:18 +0800

    Add a Bridge.component for a smoother transition from landing page to now-playing page

--- app/page.jsx
+++ app/page.jsx
@@ -4,10 +4,11 @@ import { useSearchParams } from 'next/navigation';
 
 import useAuth from '../src/hooks/useAuth';
 import Landing from './Landing';
+import Bridge from './Bridge';
 
 export default function Home() {
   const searchParams = useSearchParams();
   const accessToken = searchParams.get('access_token');
   useAuth(searchParams);
-  return <div>{accessToken ? <p>loading</p> : <Landing />}</div>;
+  return <div>{accessToken ? <Bridge /> : <Landing />}</div>;
 }
