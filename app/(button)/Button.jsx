95d29390afa1a31572dfbcc86fac1028c3a5da6d
Likai Liu <likailiu2@gmail.com>
2023-02-08 13:32:10 +0800

    Add a prop "textSize" for adjusting text size with tailwind class names

--- app/(button)/Button.jsx
+++ app/(button)/Button.jsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 
 export default function Button(props) {
-  const { content, path } = props;
-
+  const { content, path, fontSize } = props;
+  const fontSizeClass = fontSize || 'text-xl';
   return (
     <Link href={path} prefetch={false}>
       <button
-        className="btn bg-button text-primary font-bold text-xl py-2 px-4 rounded-2xl"
+        className={`btn bg-button text-primary font-bold py-2 px-4 rounded-2xl ${fontSizeClass}`}
         type="button"
         {...props}>
         {content}
