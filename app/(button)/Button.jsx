75820b1a4f9016a835ada2487c43772c6746677a
Justin <justinjdbennett@gmail.com>
2023-02-08 00:45:52 -0700

    Add some animation to Button hover

--- app/(button)/Button.jsx
+++ app/(button)/Button.jsx
@@ -6,7 +6,7 @@ export default function Button(props) {
   return (
     <Link href={path} prefetch={false}>
       <button
-        className="btn bg-button text-primary font-bold text-xl py-2 px-4 rounded-2xl hover:shadow-[0_0_8px_2px_rgba(90,79,116,0.8)]"
+        className="btn bg-button text-primary font-bold text-xl py-2 px-4 rounded-2xl button-hover-pulse"
         type="button"
         {...props}>
         {content}
