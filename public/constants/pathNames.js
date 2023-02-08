0215a413393602e3ef6a673efef49da3d05b5104
Likai Liu <113341172+Likai-L@users.noreply.github.com>
2023-02-09 05:17:54 +0800

    Add a path
    Co-authored-by: Justin <104700780+CorgiOnNeptune@users.noreply.github.com>

--- public/constants/pathNames.js
+++ public/constants/pathNames.js
@@ -1,7 +1,8 @@
 const LANDING = '/';
+const LOGIN = '/api/login';
 const NOW_PLAYING = '/now-playing';
 const SONGS = '/songs';
 const PEOPLE = '/people';
 const MESSAGES = '/messages';
 
-export { LANDING, NOW_PLAYING, SONGS, PEOPLE, MESSAGES };
+export { LANDING, LOGIN, NOW_PLAYING, SONGS, PEOPLE, MESSAGES };
