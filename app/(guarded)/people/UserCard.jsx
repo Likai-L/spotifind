510911f985736a40f3727bf09886ac103b502c58
larsyaeger <larsyaeger@gmail.com>
2023-02-08 16:52:38 -0600

    brought in from previous app/people folder

---
+++ app/(guarded)/people/UserCard.jsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+export default function UserCard() {
+  return (
+    <div className="flex max-w-[250px] min-w-[10vw] w-[15vw] m-5 flex-col bg-primary p-2 h-2/3 min-h-[20rem] rounded-lg ">
+      <div className="sticky aspect-square max-w-full min-h-2/5 h-2/4 self-center my-3">
+        <Image
+          alt="cute cat"
+          className="rounded-lg block m-auto aspect-square"
+          fill
+          src="https://thumbs.dreamstime.com/b/cute-cat-portrait-square-photo-beautiful-white-closeup-105311158.jpg"
+        />
+      </div>
+      <h2 className="text-[1.8vh] text-center cursor-default">Elon Musk</h2>
+      <h2 className="text-[1.8vh] text-center cursor-default">5.6km away</h2>
+      <button type="submit">
+        <h2 className="text-[1.8vh]">Send DM</h2>
+      </button>
+    </div>
+  );
+}
--- app/(guarded)/people/page.jsx
+++ app/(guarded)/people/page.jsx
@@ -1,5 +1,43 @@
+'use client';
+
 import SearchBar from 'app/(searchbar)/SearchBar';
+import Map from 'app/(map)/Map';
+import { useState } from 'react';
+import UserCard from './UserCard';
+import SongCard from './SongCard';
 
 export default function People() {
-  return <SearchBar action="/people" label="Search for a user" />;
+  const [showMap, setShowMap] = useState(false);
+  return (
+    <div className="w-full h-full flex-col space-y-8 overflow-hidden text-primary font-primary">
+      <h1 className="text-[1.5vw] font-primary font-semibold ml-10 cursor-default">
+        Find people with similar taste
+        <SearchBar action="/people" label="Search for a user" />
+      </h1>
+      <div className="flex m-auto w-4/5 h-1/5 rounded-3xl bg-secondary">
+        <SongCard />
+      </div>
+      <div className="container m-auto bg-secondary w-4/5 h-3/5 max-h-max rounded-3xl">
+        <div className="flex flex-row">
+          <h1 className="text-[1.5vw] p-5 cursor-default">x user/s found </h1>
+          <button
+            className="ml-[17vw]"
+            onClick={() => setShowMap(!showMap)}
+            type="submit">
+            <h2 className="text-[1.5vw]">View On Map</h2>
+          </button>
+        </div>
+        <div className="flex flex-row h-3/4 w-full overflow-x-auto overflow-y-clip items-center">
+          {showMap ? <Map /> : null}
+          {/* Will be dynamic and mapped based on search results,
+           but for now just pasted like this to see multiple */}
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+        </div>
+      </div>
+    </div>
+  );
 }
