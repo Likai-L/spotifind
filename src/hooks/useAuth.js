25ff62ef97be1dbbac2a50ca4fabf80d29afd2e2
Likai Liu <likailiu2@gmail.com>
2023-02-09 03:06:23 +0800

    Navigate to NOW-PLAYING after auth credentials are stored

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,12 +1,18 @@
 import { useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, useRouter } from 'next/navigation';
 import axios from 'axios';
 import { useGlobalContext } from 'app/(context)';
+import { NOW_PLAYING } from 'public/constants/pathNames';
 
 export default function useAuth() {
   const searchParams = useSearchParams();
+  const router = useRouter();
   const { credentials, setCredentials } = useGlobalContext();
   useEffect(() => {
+    if (credentials.accessToken) {
+      router.push('/now-playing');
+    }
+
     // since three query strings come together just check for one
     if (searchParams.get('access_token')) {
       setCredentials({
@@ -14,6 +20,7 @@ export default function useAuth() {
         refreshToken: searchParams.get('refresh_token'),
         expiresIn: searchParams.get('expires_in')
       });
+      router.push(NOW_PLAYING);
     }
   }, []);
 
