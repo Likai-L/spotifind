8a40299c0ad26ab676ae7a3e699e16cb22d42b97
Justin <justinjdbennett@gmail.com>
2023-02-07 22:34:06 -0700

    Update context spacing/comments

--- app/(context)/index.jsx
+++ app/(context)/index.jsx
@@ -1,20 +1,22 @@
 'use client';
 
 import { createContext, useContext, useMemo, useState } from 'react';
-// This basically initializes an instance of a context API
+
 export const GlobalContext = createContext();
 
 export function GlobalContextProvider(props) {
-  // state will be accessible globally, should be easy enough to link to spotify, there's a guide on connecting to existing stuff
-  // user auth credentials
+  // User Auth Credentials
   const [credentials, setCredentials] = useState({
     accessToken: null,
     refreshToken: null,
     expiresIn: null
   });
-  // spotify profile
+
+  // Spotify Profile
   const [username, setUsername] = useState('');
+
   const { children } = props;
+
   // TODO: refactor this for better state management patterns
   const globalStates = useMemo(
     () => ({
@@ -25,13 +27,14 @@ export function GlobalContextProvider(props) {
     }),
     [username, credentials.accessToken]
   );
-  // component provider, wraps around other components, components that are wrapped
-  // have access to the context username, secret, basically which account is logged in
-  // and if there is anyone logged in at all
+
+  // Component provider, wrap around other components
+  // Children have access to context
   return (
     <GlobalContext.Provider value={globalStates}>
       {children}
     </GlobalContext.Provider>
   );
 }
+
 export const useGlobalContext = () => useContext(GlobalContext);
