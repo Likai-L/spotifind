3e60a7aa8a147765c2f5f94ef34433bb6dedee95
Likai Liu <likailiu2@gmail.com>
2023-02-09 06:49:43 +0800

    Add a LandingTransition element to smooth transition between landing page (redirected back from spotify) and now-playing page

--- app/page.jsx
+++ app/page.jsx
@@ -4,10 +4,11 @@ import { useSearchParams } from 'next/navigation';
 
 import useAuth from '@/hooks/useAuth';
 import Landing from './Landing';
+import LandingTransition from './LandingTransition';
 
 export default function Home() {
   const searchParams = useSearchParams();
   const accessToken = searchParams.get('access_token');
   useAuth(searchParams);
-  return <div>{accessToken ? <p>loading</p> : <Landing />}</div>;
+  return <div>{accessToken ? <LandingTransition /> : <Landing />}</div>;
 }
