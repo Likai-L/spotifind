94f5ab566eb1cc422702c06018e85ba120f07f1a
larsyaeger <larsyaeger@gmail.com>
2023-02-07 19:10:02 -0600

    adding tailwind styling, working towards creating page with hard coded values

--- app/people/page.jsx
+++ app/people/page.jsx
@@ -1,5 +1,19 @@
+import Map from 'app/(map)/Map';
 import styles from '@/styles/Home.module.css';
 
 export default function People() {
-  return <div className={`text-green-500 ${styles.main}`}>People</div>;
+  return (
+    <div className="w-full h-full flex-col space-y-8 overflow-hidden">
+      <h1 className="text-2xl font-primary font-semibold ml-10">
+        Find people interested in:
+      </h1>
+      <div className="container m-auto w-4/5 h-1/5 rounded-3xl content-center bg-secondary" />
+      <footer className="container m-auto items-center bg-secondary w-4/5 h-3/5 max-h-max rounded-3xl content-center">
+        <div>
+          <h1 className="text-3xl p-5">x user/s found</h1>
+        </div>
+        <Map />
+      </footer>
+    </div>
+  );
 }
