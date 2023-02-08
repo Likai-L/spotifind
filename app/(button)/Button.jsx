404d8bebcae1e07369a7d60f18242e15122ccb12
Justin <justinjdbennett@gmail.com>
2023-02-08 15:47:31 -0700

    Refactor button prop handling

--- app/(button)/Button.jsx
+++ app/(button)/Button.jsx
@@ -1,15 +1,18 @@
 import Link from 'next/link';
+import classNames from 'classnames';
 
 export default function Button(props) {
-  const { content, path, fontSize, padding } = props;
-  const fontSizeClass = fontSize || 'text-xl';
-  const paddingClass = padding || 'py-2 px-4';
+  const { content, path, addedClasses } = props;
+
+  // Will take in addedClasses prop, overwriting any of the same class types set by default here.
+  const buttonClasses = classNames(
+    'btn bg-button text-primary font-primary font-bold rounded-2xl button-hover-pulse text-xl py-2 px-4',
+    addedClasses
+  );
+
   return (
     <Link href={path} prefetch={false}>
-      <button
-        className={`btn bg-button text-primary font-bold rounded-2xl ${fontSizeClass} ${paddingClass} button-hover-pulse`}
-        type="button"
-        {...props}>
+      <button className={buttonClasses} type="button" {...props}>
         {content}
       </button>
     </Link>
--- app/(button)/LoginButton.jsx
+++ app/(button)/LoginButton.jsx
@@ -5,13 +5,12 @@ import Button from './Button';
 export default function LoginButton() {
   return (
     <Button
+      addedClasses="text-3xl py-3 px-6"
       content={
         <span className="flex items-center">
           Login With <SpotifyIcon className="mx-1 ml-2" size="45" /> Spotify
         </span>
       }
-      fontSize="text-3xl"
-      padding="py-3 px-6"
       path={LOGIN}
     />
   );
