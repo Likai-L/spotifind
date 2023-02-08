ffa68c21824a4ff3c3ec5e0ffe69889d4efa479c
Likai Liu <likailiu2@gmail.com>
2023-02-09 04:00:12 +0800

    Add a path name

--- public/constants/pathNames.js
+++ public/constants/pathNames.js
@@ -2,5 +2,6 @@ const LANDING = '/';
 const NOW_PLAYING = '/now-playing';
 const SONGS = '/songs';
 const PEOPLE = '/people';
+const MESSAGES = '/messages';
 
-export { LANDING, NOW_PLAYING, SONGS, PEOPLE };
+export { LANDING, NOW_PLAYING, SONGS, PEOPLE, MESSAGES };
