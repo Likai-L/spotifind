403cc2423ce41f4ca6ecf2dc1fdca8b111b76bfb
Likai Liu <likailiu2@gmail.com>
2023-02-08 12:53:31 +0800

    Add text and logo

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,33 @@
 'use client';
 
+import Image from 'next/image';
 import LoginButton from 'app/(button)/LoginButton';
 import useAuth from '@/hooks/useAuth';
 import PeopleListening from 'app/(illustrations)/PeopleListening';
 import Phone from 'app/(illustrations)/Phone';
 import PeoplePlaying from 'app/(illustrations)/PeoplePlaying';
 import PeopleDancing from 'app/(illustrations)/PeopleDancing';
+import logo from '../public/images/logo.png';
 
 export default function Home() {
   useAuth();
 
   return (
-    <div className="bg-landing min-h-screen">
+    <div className="bg-landing min-h-screen font-primary">
       <div className="flex flex-row justify-around">
         <div>
+          <div className="text-center text-white text-5xl font-bold">
+            <p>Find Your Music Soul Mates</p>
+            <div>
+              <span>With</span>
+              <Image
+                alt="Spotifind logo"
+                src={logo}
+                className="inline-block w-20"
+              />
+              <span>SpotiFind</span>
+            </div>
+          </div>
           <LoginButton />
         </div>
 
