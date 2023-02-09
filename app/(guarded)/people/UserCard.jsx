2ff7743a0b0c6e7fe442682f681188ae7d6d7a71
Lars <115282076+larsyaeger@users.noreply.github.com>
2023-02-08 20:00:23 -0600

    Update app/(guarded)/people/UserCard.jsx
    Co-authored-by: Justin <104700780+CorgiOnNeptune@users.noreply.github.com>

--- app/(guarded)/people/UserCard.jsx
+++ app/(guarded)/people/UserCard.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function UserCard() {
   return (
-    <div className="flex max-w-[250px] min-w-[10vw] w-[15vw] m-5 flex-col bg-primary p-2 h-2/3 min-h-[20rem] rounded-lg ">
+    <div className="flex flex-col justify-evenly max-w-[250px] min-w-[10vw] w-[15vw] m-5 bg-primary p-2 h-2/3 min-h-[20rem] rounded-lg ">
       <div className="sticky aspect-square max-w-full min-h-2/5 h-2/4 self-center my-3">
         <Image
           alt="cute cat"
@@ -11,8 +11,8 @@ export default function UserCard() {
           src="https://thumbs.dreamstime.com/b/cute-cat-portrait-square-photo-beautiful-white-closeup-105311158.jpg"
         />
       </div>
-      <h2 className="text-[1.8vh] text-center cursor-default">Elon Musk</h2>
-      <h2 className="text-[1.8vh] text-center cursor-default">5.6km away</h2>
+      <h2 className="text-[1.8vh] text-center font-bold cursor-default">Elons' Musk</h2>
+      <h2 className="text-[1.8vh] text-center text-secondary cursor-default">5.6km away</h2>
       <button type="submit">
         <h2 className="text-[1.8vh]">Send DM</h2>
       </button>
