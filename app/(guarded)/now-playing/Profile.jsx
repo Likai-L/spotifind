e58e1beb4b9e9cf57175002e42448d9dda25bb47
Likai Liu <likailiu2@gmail.com>
2023-02-09 17:02:19 +0800

    Use profile states to dynamically render profile

--- app/(guarded)/now-playing/Profile.jsx
+++ app/(guarded)/now-playing/Profile.jsx
@@ -1,16 +1,19 @@
 import Image from 'next/image';
+import { useGlobalContext } from 'app/(context)';
+
 export default function Profile() {
+  const { profile } = useGlobalContext();
   return (
     <div className="text-center w-[200px]">
       <Image
         alt="pfp"
-        src="https://i.scdn.co/image/ab67616d0000b273d0a93f4d6d61ec9f850218f9"
+        src={profile.avatar}
         width={200}
         height={200}
         className="rounded-3xl"
       />
-      <div className="text-2xl mt-4">Grimes</div>
-      <div className="mt-1 opacity-30">@GrimesGurlll</div>
+      <div className="text-2xl mt-4">{profile.name}</div>
+      <div className="mt-1 opacity-30">@{profile.handle}</div>
     </div>
   );
 }
