b7e485d17cc7cbc98d7dc886eaf98894e295a19a
Likai Liu <likailiu2@gmail.com>
2023-02-09 16:29:06 +0800

    Add spotify API base url

--- public/constants/pathNames.js
+++ public/constants/pathNames.js
@@ -4,5 +4,14 @@ const NOW_PLAYING = '/now-playing';
 const SONGS = '/songs';
 const PEOPLE = '/people';
 const MESSAGES = '/messages';
+const SPOTIFY_ENDPOINT = 'https://api.spotify.com/v1';
 
-export { LANDING, LOGIN, NOW_PLAYING, SONGS, PEOPLE, MESSAGES };
+export {
+  LANDING,
+  LOGIN,
+  NOW_PLAYING,
+  SONGS,
+  PEOPLE,
+  MESSAGES,
+  SPOTIFY_ENDPOINT
+};
