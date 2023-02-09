2e8d7e9853fd3edca808482bc6b92a00f20916b4
Justin <justinjdbennett@gmail.com>
2023-02-08 18:53:29 -0700

    Band-aid fix to CORS...

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
   // },
   experimental: {
     appDir: true
+  },
+  // TODO: Fix CORS issues.......
+  async rewrites() {
+    return [
+      {
+        source: '/api/login',
+        destination: '/'
+      }
+    ];
   }
 };
 
