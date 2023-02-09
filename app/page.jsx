d8d8b9afdd1a7115703dc4d9c578b6baa041a003
larsyaeger <larsyaeger@gmail.com>
2023-02-08 18:46:50 -0600

    revert commit a4652333afa5a860380f4672ce330941b1612ee4

--- app/page.jsx
+++ app/page.jsx
@@ -4,11 +4,10 @@ import { useSearchParams } from 'next/navigation';
 
 import useAuth from '../src/hooks/useAuth';
 import Landing from './Landing';
-import Bridge from './Bridge';
 
 export default function Home() {
   const searchParams = useSearchParams();
   const accessToken = searchParams.get('access_token');
   useAuth(searchParams);
-  return <div>{accessToken ? <Bridge /> : <Landing />}</div>;
+  return <div>{accessToken ? <p>loading</p> : <Landing />}</div>;
 }
