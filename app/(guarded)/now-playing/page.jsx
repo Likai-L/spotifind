d15904fdb58129a5a5fae6f928d469dd2cf2dfe6
Likai Liu <likailiu2@gmail.com>
2023-02-10 11:41:24 +0800

    Add Player component to the now playing page

--- app/(guarded)/now-playing/page.jsx
+++ app/(guarded)/now-playing/page.jsx
@@ -1,9 +1,11 @@
 import Profile from './Profile';
+import Player from './Player';
 
 export default function NowPlaying() {
   return (
     <div>
       <Profile />
+      <Player />
     </div>
   );
 }
