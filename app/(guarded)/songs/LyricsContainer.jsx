8334b306ac57cb0ae51674531bc713244f250bfb
Justin <justinjdbennett@gmail.com>
2023-02-09 21:19:04 -0700

    Lyrics inital, needs adjusting still

---
+++ app/(guarded)/songs/LyricsContainer.jsx
@@ -0,0 +1,62 @@
+export default function LyricsContainer(/* props */) {
+  // const { songLyrics } = props;
+
+  return (
+    <div className="h-1/4">
+      <div className="text-3xl font-bold m-8">Lyrics</div>
+      <div className="flex flex-col justify-start items-center font-semibold text-xl bg-secondary h-full text-md rounded-3xl overflow-y-scroll">
+        <p className="text-center p-4">
+          I was so tender
+          <br />
+          I was so tender
+          <br />
+          Now born the same
+          <br />
+          And born lost forever
+          <br />
+          I said; Born forever
+          <br />
+          I said; Born together again
+          <br />
+          I said
+          <br />
+          So sensitive
+          <br />
+          So tender
+          <br />
+          I’ll say
+          <br />
+          <br />
+          She tranquilizes with tranquil eyes
+          <br />
+          <br />
+          I’ll try not to lose you
+          <br />
+          Ill never lose heart again
+          <br />
+          (When can I go back)
+          <br />
+          <br />
+          Where have you all gone anyway?
+          <br />
+          It’s not the scene without you
+          <br />
+          I’ll say
+          <br />
+          <br />
+          God, what is this new place where my impulse can be found
+          <br />
+          Fall down the spiral staircase where my friends can be found
+          <br />
+          <br />
+          How can I just stay home?
+          <br />
+          When can I go back to the broken city?
+          <br />
+          (When can I go back)
+        </p>
+        <br />
+      </div>
+    </div>
+  );
+}
