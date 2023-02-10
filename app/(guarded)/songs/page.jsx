2d726a6e32b806ebc080f7035d5f69ec4b7a1a23
Justin <justinjdbennett@gmail.com>
2023-02-10 16:46:49 -0700

    recommended component setup

--- app/(guarded)/songs/page.jsx
+++ app/(guarded)/songs/page.jsx
@@ -1,14 +1,15 @@
 import SearchBar from 'app/(searchbar)/SearchBar';
 import { SONGS } from 'public/constants/pathNames';
 import SongContainer from './SongContainer';
+import Recommended from './Recommended';
 
-export default function Home() {
+export default function Songs() {
   return (
     <div className="flex flex-col justify-center h-[90vh] w-[80vw]">
       <div className="flex justify-center pb-8">
         <SearchBar action={SONGS} label="Search for a song" />
       </div>
-      <SongContainer />
+      {<Recommended /> || <SongContainer />}
     </div>
   );
 }
