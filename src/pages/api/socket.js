2bc5aa240f8d3bcebf2b202d39f1d6044af94bd6
Likai Liu <likailiu2@gmail.com>
2023-02-11 05:54:50 +0800

    Major bugfix: use socket.emit instead of socket.broadcast.emit because the latter doesn't emit to the sender, which the client is because it sent the access token

--- src/pages/api/socket.js
+++ src/pages/api/socket.js
@@ -8,64 +8,58 @@ import { sortPlayerStateData } from '@/helpers/socketHelper';
 export default function SocketHandler(req, res) {
   if (res.socket.server.io) {
     console.log('Socket is already running');
-  } else {
-    console.log('Socket is initializing');
-    const io = new Server(res.socket.server);
-    res.socket.server.io = io;
-    io.on('connection', socket => {
-      socket.on('access-token', token => {
-        socket.token = token;
-        console.log(socket.token);
-      });
-      socket.poll = () => {
-        axios(`${SPOTIFY_ENDPOINT}/me/player`, getHeaders(socket.token))
-          .then(response => {
-            const playerState = sortPlayerStateData(response);
-            if (!playerState) {
-              console.log('no data came back!');
-              // no active player
-              if (socket.playerState) {
-                socket.playerState = null;
-                socket.broadcast.emit('no-active', null);
-              }
-            } else if (!socket.playerState) {
-              // set initial player state and send it to the client
-              socket.playerState = playerState;
-              socket.broadcast.emit('initial-state', playerState);
-            } else {
-              // compare and send changes to the client
-              console.log(socket.playerState.uri);
-              console.log(playerState.uri);
-              if (socket.playerState.device !== playerState.device) {
-                socket.playerState.device = playerState.device;
-                socket.broadcast.emit(
-                  'device-change',
-                  socket.playerState.device
-                );
-              }
-              if (socket.playerState.uri !== playerState.uri) {
-                socket.playerState = playerState;
-                socket.broadcast.emit('track-change', socket.playerState);
-              }
-              if (socket.playerState.isPlaying !== playerState.isPlaying) {
-                socket.playerState.isPlaying = playerState.isPlaying;
-                socket.broadcast.emit(
-                  'play-status-change',
-                  socket.playerState.isPlaying
-                );
-              }
-            }
-
-            setTimeout(socket.poll, 2000);
-          })
-          .catch(err => {
-            console.log(err);
-            setTimeout(socket.poll, 2000);
-          });
-      };
-      socket.poll();
-    });
+    res.end();
+    return;
   }
+  console.log('Socket is initializing');
+  const io = new Server(res.socket.server);
+  res.socket.server.io = io;
+  io.on('connection', socket => {
+    socket.on('access-token', token => {
+      socket.token = token;
+      console.log(socket.token);
+    });
+    socket.poll = () => {
+      axios(`${SPOTIFY_ENDPOINT}/me/player`, getHeaders(socket.token))
+        .then(response => {
+          const playerState = sortPlayerStateData(response);
+          if (!playerState) {
+            console.log('no data came back!');
+            // no active player
+            if (socket.playerState) {
+              socket.playerState = null;
+              socket.emit('no-active', null);
+            }
+          } else if (!socket.playerState) {
+            // set initial player state and send it to the client
+            socket.playerState = playerState;
+            socket.emit('initial-state', playerState);
+          } else {
+            // compare and send changes to the client
+            console.log(socket.playerState.uri);
+            console.log(playerState.uri);
+            if (socket.playerState.device !== playerState.device) {
+              socket.playerState.device = playerState.device;
+              socket.emit('device-change', socket.playerState.device);
+            }
+            if (socket.playerState.uri !== playerState.uri) {
+              socket.playerState = playerState;
+              socket.emit('track-change', socket.playerState);
+            }
+            if (socket.playerState.isPlaying !== playerState.isPlaying) {
+              socket.playerState.isPlaying = playerState.isPlaying;
+              socket.emit('play-status-change', socket.playerState.isPlaying);
+            }
+          }
 
+          setTimeout(socket.poll, 2000);
+        })
+        .catch(err => {
+          console.log(err);
+          setTimeout(socket.poll, 2000);
+        });
+    };
+    socket.poll();
+  });
   res.end();
 }
