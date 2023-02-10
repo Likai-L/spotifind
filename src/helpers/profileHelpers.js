b6c0258a17d65ae3a57a7ad31d2543b11e35c328
Likai Liu <likailiu2@gmail.com>
2023-02-10 11:41:11 +0800

    Update data processing following endpoint change

--- src/helpers/profileHelpers.js
+++ src/helpers/profileHelpers.js
@@ -14,17 +14,17 @@ export const getHeaders = accessToken => {
 
 /**
  * process top tracks data
- * @param   {[]} tracks array of tracks from the top items endpoint
+ * @param   {[]} tracks array of tracks from the saved tracks
  * @return  {[]}      an array of top tracks with needed info
  */
-export const getTopTracks = tracks => {
+export const getTracks = tracks => {
   const topTracks = tracks.map(track => {
     return {
-      uri: track.id,
-      trackName: track.name,
-      artist: track.artists[0],
-      albumName: track.album.name,
-      posterUrl: track.album.images[0].url
+      uri: track.track.id,
+      trackName: track.track.name,
+      artist: track.track.artists[0],
+      albumName: track.track.album.name,
+      posterUrl: track.track.album.images[0].url
     };
   });
   return topTracks;
