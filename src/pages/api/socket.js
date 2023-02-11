c30f627d5d6806091c938e40fbcd8d8c85eae28f
Likai Liu <likailiu2@gmail.com>
2023-02-11 08:32:35 +0800

    Change the message sent if no data comes back

--- src/pages/api/socket.js
+++ src/pages/api/socket.js
@@ -24,18 +24,17 @@ export default function SocketHandler(req, res) {
         .then(response => {
           const playerState = sortPlayerStateData(response);
           if (!playerState) {
-            console.log('no data came back!');
             // no active player
             if (socket.playerState) {
-              socket.playerState = null;
-              socket.emit('no-active', null);
+              socket.playerState = { noActiveDevice: true };
+              socket.emit('no-active', socket.playerState);
             }
           } else if (!socket.playerState) {
             // set initial player state and send it to the client
             socket.playerState = playerState;
             socket.emit('initial-state', playerState);
           } else {
-            // compare and send changes to the client
+            // compare and send updates to the client
             console.log(socket.playerState.uri);
             console.log(playerState.uri);
             if (socket.playerState.device !== playerState.device) {
