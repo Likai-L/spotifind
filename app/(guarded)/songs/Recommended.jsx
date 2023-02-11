21e46dd1a04f2bc6e7602280e20c4d11ced2b9f1
Justin <justinjdbennett@gmail.com>
2023-02-11 00:44:21 -0700

    Setup album grid for recommended tracks

--- app/(guarded)/songs/Recommended.jsx
+++ app/(guarded)/songs/Recommended.jsx
@@ -1,8 +1,21 @@
+'use client';
+
+import { useGlobalContext } from 'app/(context)';
+import AlbumGrid from './AlbumGrid';
+
 export default function Recommended() {
+  const { recommendedTracks } = useGlobalContext();
+
   return (
     <>
-      <p className="text-4xl font-bold my-[30px]">Recommended</p>
-      <div />
+      <div className="text-4xl font-bold my-[30px]">Recommended</div>
+      {recommendedTracks.length > 0 ? (
+        <AlbumGrid tracks={recommendedTracks} />
+      ) : (
+        <div className="flex justify-center items-center h-full text-3xl font-bold">
+          Unable to find recommendations.
+        </div>
+      )}
     </>
   );
 }
