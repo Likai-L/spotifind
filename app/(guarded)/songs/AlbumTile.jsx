f6e62932eb5a2e2c82e75705e2d85d5d5e32a324
Justin <justinjdbennett@gmail.com>
2023-02-10 20:01:08 -0700

    AlbumTile component for song searches

---
+++ app/(guarded)/songs/AlbumTile.jsx
@@ -0,0 +1,15 @@
+import AlbumCover from '../now-playing/AlbumCover';
+
+export default function AlbumTile({ track: { posterUrl, trackName, artist } }) {
+  return (
+    <div className="flex flex-col items-center font-primary p-8">
+      <AlbumCover classNames="h-[12rem] w-[12rem]" src={posterUrl} />
+      <p className="font-bold text-2xl text-primary pt-2 pb-1 text-center">
+        {trackName}
+      </p>
+      <p className="font-semibold text-lg text-secondary pt-1 pb-2 text-center">
+        {artist}
+      </p>
+    </div>
+  );
+}
