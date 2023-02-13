1b1fe7979960f1dcafa9db8488c55fc24e92ea98
larsyaeger <larsyaeger@gmail.com>
2023-02-12 22:51:08 -0600

    added some logic

--- app/(guarded)/people/UserSearch.jsx
+++ app/(guarded)/people/UserSearch.jsx
@@ -1,9 +1,29 @@
 'use client';
 
+import React, { useState } from 'react';
+
 export default function UserSearch() {
+  const [title, setTitle] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    const searched = { title };
+    // make request to database to find a user
+    fetch('', {
+      method: 'GET'
+    });
+  };
   return (
     <div className="">
-      <input placeholder="Search for a user" type="text" />
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={e => setTitle(e.target.value)}
+          required
+          type="text"
+          value={title}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {title}
     </div>
   );
 }
