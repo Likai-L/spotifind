800ecb6cf471fad342b6973f6b48654b351c19ca
Likai Liu <likailiu2@gmail.com>
2023-02-13 08:10:24 +0800

    Now /api/profile only handles profile create/update

--- src/pages/api/profile.js
+++ src/pages/api/profile.js
@@ -1,38 +1,14 @@
 import prisma from '@/helpers/prisma';
 
 export default async function profileHandler(req, res) {
-  const newUser = req.body;
-  const user = await prisma.user.findUnique({
-    where: {
-      spotifyUserUri: newUser.spotifyUserUri
-    }
+  const user = req.body;
+  // if user found, update; if not, create
+  const updatedUser = await prisma.user.upsert({
+    where: { spotifyUserUri: user.spotifyUserUri },
+    update: user,
+    create: user
   });
-  // no such user found in db
-  if (!user) {
-    console.log('🚨🚨🚨No such user found, creating...');
-    await prisma.user.create({ data: newUser }); // rest already contain sorted user data
-    console.log('New user created', newUser);
-    res.end();
-    return;
-  }
-  // user exists, check if anything is changed
-  if (user.profilePictureUrl !== newUser.profilePictureUrl) {
-    console.log('🚨🚨🚨pfp chganged, updating...');
-    await prisma.user.update({
-      where: { spotifyUserUri: user.profilePictureUrl },
-      data: { profilePictureUrl: newUser.profilePictureUrl }
-    });
-    console.log('profile picture updated');
-  }
-
-  if (user.currentSongUri !== newUser.currentSongUri) {
-    console.log('🚨🚨🚨current song changed, updating...');
-    await prisma.user.update({
-      where: { spotifyUserUri: user.profilePictureUrl },
-      data: { currentSongUri: newUser.currentSongUri }
-    });
-  }
-  console.log('Playing song updated:', newUser.currentSongUri);
+  console.log(updatedUser);
 
   res.end();
 }
