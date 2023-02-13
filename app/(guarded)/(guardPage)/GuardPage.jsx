fe6037343ddf2bff6743a77308e52a46cc49b6f9
Justin <justinjdbennett@gmail.com>
2023-02-13 00:29:05 -0700

    Commented presentation bugfix thing with socket stuff

--- app/(guarded)/(guardPage)/GuardPage.jsx
+++ app/(guarded)/(guardPage)/GuardPage.jsx
@@ -4,10 +4,16 @@ import { useGlobalContext } from 'app/(context)';
 import { useRouter } from 'next/navigation';
 
 import { LANDING } from 'public/constants/pathNames';
+// import useCurrentTrack from '@/hooks/useCurrentTrack';
 
 export default function GuardPage({ children }) {
   const { credentials } = useGlobalContext();
   const router = useRouter();
+
+  // Uncomment useCurrentTrack() to hide axios errors to api/socket in
+  // /people when we do our presentation.
+  // useCurrentTrack();
+
   if (!credentials.accessToken) {
     router.push(LANDING);
   }
