166dcb71838060a76f1b61960e0dda89b9f87cb5
larsyaeger <larsyaeger@gmail.com>
2023-02-13 03:12:06 -0600

    on submit, getUserByName function fires and passes in typed value as a parameter

--- app/(guarded)/people/UserSearch.jsx
+++ app/(guarded)/people/UserSearch.jsx
@@ -1,32 +1,26 @@
 'use client';
 
 import { useState } from 'react';
-import axios from 'axios';
-import { USERSEARCH } from 'public/constants.pathNames';
+import getUserByName from '@/helpers/getUserByName';
 
 export default function UserSearch() {
   const [title, setTitle] = useState('');
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault();
     const search = { title };
     // make request to database to find a user with that name!
-    axios
-      .post(USERSEARCH, {
-        name: search
-      })
-      .then(res => console.log(res))
-      .catch(err => console.log(err));
+    getUserByName(search);
   };
   return (
     <div className="">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={() => handleSubmit}>
         <input
           onChange={e => setTitle(e.target.value)}
           required
           type="text"
           value={title}
         />
-        <button type="submit">Submit</button>
+        <button type="button">Submit</button>
       </form>
       {title}
     </div>
