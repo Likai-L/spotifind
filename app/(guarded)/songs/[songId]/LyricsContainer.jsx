17588adb7649eb62bfe3c6293e78030d88e35b19
Likai Liu <likailiu2@gmail.com>
2023-02-14 06:24:41 +0800

    Change styles

--- app/(guarded)/songs/[songId]/LyricsContainer.jsx
+++ app/(guarded)/songs/[songId]/LyricsContainer.jsx
@@ -5,13 +5,13 @@ export default function LyricsContainer(props) {
   const { songLyrics } = props;
 
   return (
-    <div className="h-3/4">
+    <div className="h-3/4 flex flex-col justify-evenly">
       <div className="flex justify-between">
         <div className="text-3xl font-bold my-6">Lyrics</div>{' '}
         <MusixMatch className="mr-32" width={150} />
       </div>
-      <Container classNames="bg-container-light max-w-[90%] min-h-[400px] overflow-auto scrollbar-hide">
-        <div className="w-100 h-4/5 text-center text-xl font-semibold leading-loose py-4">
+      <Container classNames="bg-container-light max-w-[90%] min-h-[400px] overflow-hidden">
+        <div className="w-100 h-full text-center text-xl font-semibold leading-loose overflow-scroll scrollbar-hide py-[50px]">
           <p className="whitespace-pre-line">
             {songLyrics || 'No lyrics available'}
           </p>
