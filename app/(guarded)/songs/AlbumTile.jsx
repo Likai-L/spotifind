1ef21d19640e6d98e48a678e7d9b437d15f6ba71
Justin <justinjdbennett@gmail.com>
2023-02-12 22:59:54 -0700

    Fix some image console errors

--- app/(guarded)/songs/AlbumTile.jsx
+++ app/(guarded)/songs/AlbumTile.jsx
@@ -1,25 +1,17 @@
-import Link from 'next/link';
 import AlbumCover from '../now-playing/AlbumCover';
 
 export default function AlbumTile({
   track: { albumCoverUrl, trackName, artist, uri }
 }) {
   return (
-    <Link href={`/songs/${uri}`}>
-      <div className="flex flex-col items-center font-primary p-8">
-        <AlbumCover
-          height={200}
-          src={albumCoverUrl}
-          trackUri={uri}
-          width={200}
-        />
-        <p className="font-bold text-2xl text-primary pt-2 pb-1 text-center  line-clamp-2">
-          {trackName}
-        </p>
-        <p className="font-semibold text-lg text-secondary pt-1 pb-2 text-center">
-          {artist}
-        </p>
-      </div>
-    </Link>
+    <div className="flex flex-col items-center font-primary p-8">
+      <AlbumCover height={200} src={albumCoverUrl} trackUri={uri} width={200} />
+      <p className="font-bold text-2xl text-primary pt-2 pb-1 text-center  line-clamp-2">
+        {trackName}
+      </p>
+      <p className="font-semibold text-lg text-secondary pt-1 pb-2 text-center">
+        {artist}
+      </p>
+    </div>
   );
 }
