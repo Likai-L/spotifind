502eb0f218374436c2bd5265f4fc88005201c447
larsyaeger <larsyaeger@gmail.com>
2023-02-12 22:26:07 -0600

    fixed syntax

--- src/pages/api/location.js
+++ src/pages/api/location.js
@@ -2,22 +2,21 @@ import { useGlobalContext } from 'app/(context)';
 import prisma from '@/helpers/prisma';
 
 export default async function locationHandler(req, res) {
-  const currentUser = req.body;
+  const user = req.body;
   if (useGlobalContext.longitude !== '' && useGlobalContext.latitude !== '') {
-    await prisma.User.update({
-      where: { spotifyUserUri: currentUser.spotifyUserUri },
-      data: {
-        upsert: {
-          create: {
-            longitude: useGlobalContext.longitude,
-            latitude: useGlobalContext.latitude
-          },
-          update: {
-            longitude: useGlobalContext.longitude,
-            latitude: useGlobalContext.latitude
-          }
-        }
+    const updatedUser = await prisma.user.upsert({
+      where: { spotifyUserUri: user.spotifyUserUri },
+      create: {
+        longitude: useGlobalContext.longitude,
+        latitude: useGlobalContext.latitude
+      },
+      update: {
+        longitude: useGlobalContext.longitude,
+        latitude: useGlobalContext.latitude
       }
     });
+    console.log(updatedUser);
+
+    res.end();
   }
 }
