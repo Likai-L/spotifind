031c3385ba2e20e0f86275185f74432c263138fd
Likai Liu <likailiu2@gmail.com>
2023-02-14 17:12:26 +0800

    Replace hard-coded comments with an array of Comment components with dynamic data

--- app/(guarded)/songs/[songId]/CommentsContainer.jsx
+++ app/(guarded)/songs/[songId]/CommentsContainer.jsx
@@ -1,15 +1,15 @@
 import Container from 'app/(container)/Container';
+import { useGlobalContext } from 'app/(context)';
 import Comment from './Comment';
 
-export default function CommentsContainer(props) {
+export default function CommentsContainer() {
+  const { comments } = useGlobalContext();
+  const commentsArray = comments.map(comment => <Comment {...comment} />);
   return (
     <div className="h-3/4">
       <div className="text-3xl font-bold my-6">Comments</div>
       <Container classNames="bg-container-light max-w-[90%] min-h-[600px] overflow-auto scrollbar-hide">
-        <Comment>Yes gurl you go slayyyy</Comment>
-        <Comment>Yes gurl you go slayyyy</Comment>
-        <Comment>Yes gurl you go slayyyy</Comment>
-        <Comment>Yes gurl you go slayyyy</Comment>
+        {commentsArray}
       </Container>
     </div>
   );
