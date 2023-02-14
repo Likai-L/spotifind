618dc98d2ce6b9ff8a671f31f88bd6a8c10bbe73
Likai Liu <likailiu2@gmail.com>
2023-02-15 05:42:28 +0800

    Add nested children comments

--- app/(guarded)/songs/[songId]/Comment.jsx
+++ app/(guarded)/songs/[songId]/Comment.jsx
@@ -3,49 +3,82 @@ import { FaTrash, FaEdit, FaHeart, FaReply } from 'react-icons/fa';
 import formatDistance from 'date-fns/formatDistance';
 import Container from 'app/(container)/Container';
 import IconButton from './IconButton';
+import CommentList from './CommentList';
 
-export default function Comment(props) {
-  const { author, content, createdAt } = props;
+export default function Comment({
+  author,
+  content,
+  createdAt,
+  id,
+  getReplies
+}) {
+  const childComments = getReplies(id);
+  const childrenHidden = false;
   return (
-    <div className="h-[150px]] flex justify-center my-[20px]">
-      <div className="flex flex-col justify-between mr-[20px] w-[80px]">
-        <Image
-          alt="user-pfp"
-          className="rounded-2xl cursor-pointer transition duration-200 ease-out hover:scale-105"
-          height={80}
-          src={author?.profilePictureUrl || ''}
-          width={80}
-        />
-        <p className="text-md text-center w-full truncate">
-          {author?.username || ''}
-        </p>
-      </div>
-      <Container classNames="bg-container px-[25px] py-[15px] w-[80%] h-[120px] overflow-auto scrollbar-hide flex flex-col justify-between">
-        <p>{content || ''}</p>
-        <div className="flex justify-between">
-          <p className="text-secondary text-base">
-            {formatDistance(new Date(createdAt), new Date(), {
-              addSuffix: true
-            })}
+    <>
+      <div className="h-[150px]] flex justify-center mt-[20px]">
+        <div className="flex flex-col justify-between mr-[20px] w-[80px]">
+          <Image
+            alt="user-pfp"
+            className="rounded-2xl cursor-pointer transition duration-200 ease-out hover:scale-105"
+            height={80}
+            src={author?.profilePictureUrl || ''}
+            width={80}
+          />
+          <p className="text-md text-center w-full truncate">
+            {author?.username || ''}
           </p>
-          <div className="flex justify-between">
-            <IconButton aria-label="Like" Icon={FaHeart}>
-              2
-            </IconButton>
-            <IconButton aria-label="Reply" Icon={FaReply} />
-            <IconButton
-              aria-label="Edit"
-              hover="hover:text-blue-400"
-              Icon={FaEdit}
-            />
-            <IconButton
-              aria-label="Delete"
-              hover="hover:text-red-400"
-              Icon={FaTrash}
-            />
-          </div>
         </div>
-      </Container>
-    </div>
+        <Container classNames="bg-container px-[25px] py-[15px] w-[720px] h-[120px] overflow-auto scrollbar-hide flex flex-col justify-between">
+          <p>{content || ''}</p>
+          <div className="flex justify-between">
+            <p className="text-secondary text-base">
+              {formatDistance(new Date(createdAt), new Date(), {
+                addSuffix: true
+              })}
+            </p>
+            <div className="flex justify-between">
+              <IconButton
+                aria-label="Like"
+                hover="hover:text-pink-300"
+                Icon={FaHeart}>
+                2
+              </IconButton>
+              <IconButton
+                aria-label="Reply"
+                hover="hover:text-green-300"
+                Icon={FaReply}
+              />
+              <IconButton
+                aria-label="Edit"
+                hover="hover:text-blue-300"
+                Icon={FaEdit}
+              />
+              <IconButton
+                aria-label="Delete"
+                hover="hover:text-red-400"
+                Icon={FaTrash}
+              />
+            </div>
+          </div>
+        </Container>
+      </div>
+      <div>
+        {childComments?.length > 0 && (
+          <>
+            <div className=" flex justify-center">
+              <button
+                aria-label="Hide Replies"
+                className="collapse-line  w-[30px]  relative"></button>
+              <div>
+                <CommentList
+                  comments={childComments}
+                  getReplies={getReplies}></CommentList>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
