f2207d6d9dc6ceffb369f606e54794485d5fb7e7
larsyaeger <larsyaeger@gmail.com>
2023-02-13 23:08:20 -0600

    still ugly, but uses map instead of a loop

--- app/(guarded)/people/UserCardRenderer.jsx
+++ app/(guarded)/people/UserCardRenderer.jsx
@@ -7,7 +7,7 @@ import UserCard from './UserCard';
 // import getUsersBySong from '@/helpers/getUsersBySong';
 
 export default function UserCardRenderer({ track }) {
-  const { uri, albumCoverUrl, trackName, artistName, albumName } = track;
+  const { uri } = track;
   const [userCards, setUserCards] = useState('');
   useEffect(() => {
     axios
@@ -19,19 +19,22 @@ export default function UserCardRenderer({ track }) {
   }, []);
   console.log('usercards object under here', userCards);
   console.log(userCards);
-
+  // I know it's best practice to put this straight in the return statement and forego the const userCardsComponent,
+  // but this caused it not to render in this instance, maybe because its an api call?
   if (userCards.length > 0) {
-    const userCardsComponent = [];
-    for (const userCard of userCards) {
-      userCardsComponent.push(
-        <UserCard
-          alt={userCard.username}
-          distance="4km"
-          image={userCard.profilePictureUrl}
-          name={userCard.username}
-        />
-      );
-    }
-    return <div>{userCardsComponent}</div>;
+    const userCardsComponent = userCards.map(userCard => (
+      <UserCard
+        alt={`${userCard.username}'s profile photo`}
+        distance="4km"
+        image={userCard.profilePictureUrl}
+        key={userCard.uri}
+        name={userCard.username}
+      />
+    ));
+    return (
+      <div className="flex flex-row h-3/4 w-full m-auto overflow-x-auto overflow-y-hidden scrollbar-hide items-center">
+        {userCardsComponent}
+      </div>
+    );
   }
 }
