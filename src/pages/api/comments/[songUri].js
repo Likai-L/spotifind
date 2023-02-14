4d5a1c43dbc85f69cbb4b44f260dc66f69515ce2
Likai Liu <likailiu2@gmail.com>
2023-02-14 15:23:54 +0800

    Create an api route for retrieving comments of a song from the db

---
+++ src/pages/api/comments/[songUri].js
@@ -0,0 +1,26 @@
+import prisma from '@/helpers/prisma';
+
+export default async function songCommentsHandler(req, res) {
+  const { songUri } = req.query;
+  console.log('Inside songComments api, got song Uri:', songUri);
+  const comments = await prisma.comment.findMany({
+    orderBy: {
+      createdAt: 'desc'
+    },
+    where: { songUri },
+    select: {
+      id: true,
+      content: true,
+      parentId: true,
+      createdAt: true,
+      author: {
+        select: {
+          spotifyUserUri: true,
+          username: true,
+          profilePictureUrl: true
+        }
+      }
+    }
+  });
+  res.send(comments);
+}
