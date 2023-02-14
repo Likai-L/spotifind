d5b802d38f039c499f0234933feca65ae8a79431
Likai Liu <likailiu2@gmail.com>
2023-02-14 17:41:01 +0800

    Add formatted time distance to comment

--- app/(guarded)/songs/[songId]/Comment.jsx
+++ app/(guarded)/songs/[songId]/Comment.jsx
@@ -1,8 +1,9 @@
 import Container from 'app/(container)/Container';
 import Image from 'next/image';
+import formatDistance from 'date-fns/formatDistance';
 
 export default function Comment(props) {
-  const { author, content } = props;
+  const { author, content, createdAt } = props;
   return (
     <div className="h-[150px]] flex justify-center my-[20px]">
       <div className="flex flex-col justify-between mr-[20px] w-[80px]">
@@ -17,8 +18,15 @@ export default function Comment(props) {
           {author?.username || ''}
         </p>
       </div>
-      <Container classNames="bg-container p-[20px] w-[80%] h-[120px] overflow-auto scrollbar-hide">
+      <Container classNames="bg-container px-[25px] py-[15px] w-[80%] h-[120px] overflow-auto scrollbar-hide flex flex-col justify-between">
         <p>{content || ''}</p>
+        <div>
+          <p className="text-secondary text-base">
+            {formatDistance(new Date(createdAt), new Date(), {
+              addSuffix: true
+            })}
+          </p>
+        </div>
       </Container>
     </div>
   );
