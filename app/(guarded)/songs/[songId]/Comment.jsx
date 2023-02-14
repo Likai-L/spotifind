38e3a0fadff801bd1f506851c5fff973513e111e
Likai Liu <likailiu2@gmail.com>
2023-02-14 17:11:37 +0800

    Render comments

--- app/(guarded)/songs/[songId]/Comment.jsx
+++ app/(guarded)/songs/[songId]/Comment.jsx
@@ -2,22 +2,23 @@ import Container from 'app/(container)/Container';
 import Image from 'next/image';
 
 export default function Comment(props) {
+  const { author, content } = props;
   return (
     <div className="h-[150px]] flex justify-center my-[20px]">
-      <div className="flex flex-col justify-between mr-[20px]">
+      <div className="flex flex-col justify-between mr-[20px] w-[80px]">
         <Image
           alt="user-pfp"
-          className={
-            'rounded-2xl cursor-pointer transition duration-200 ease-out hover:scale-105'
-          }
+          className="rounded-2xl cursor-pointer transition duration-200 ease-out hover:scale-105"
           height={80}
-          src="https://i.scdn.co/image/ab6775700000ee852dcd78968e2f994406a4160c"
+          src={author?.profilePictureUrl || ''}
           width={80}
         />
-        <p className="text-lg text-center">Kai</p>
+        <p className="text-md text-center w-full truncate">
+          {author?.username || ''}
+        </p>
       </div>
       <Container classNames="bg-container p-[20px] w-[80%] h-[120px] overflow-auto scrollbar-hide">
-        <p>{props.children}</p>
+        <p>{content || ''}</p>
       </Container>
     </div>
   );
