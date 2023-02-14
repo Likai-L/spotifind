d038c2b0889571c82a1f58c2372574eea9cdec6c
larsyaeger <larsyaeger@gmail.com>
2023-02-13 22:44:33 -0600

    got working in buggy state, currently the code AND the rendering looks ugly as heck

--- app/(guarded)/people/UserCardRenderer.jsx
+++ app/(guarded)/people/UserCardRenderer.jsx
@@ -1,8 +1,37 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { FINDUSERSBYSONG } from 'public/constants/pathNames';
 import UserCard from './UserCard';
-import getUsersBySong from '@/helpers/getUsersBySong';
+// import getUsersBySong from '@/helpers/getUsersBySong';
 
 export default function UserCardRenderer({ track }) {
   const { uri, albumCoverUrl, trackName, artistName, albumName } = track;
-  getUsersBySong(uri);
-  return <UserCard distance="3km" name="Bob" />;
+  const [userCards, setUserCards] = useState('');
+  useEffect(() => {
+    axios
+      .post(FINDUSERSBYSONG, {
+        currentSongUri: uri
+      })
+      .then(res => setUserCards(res.data))
+      .catch(err => console.log(err));
+  }, []);
+  console.log('usercards object under here', userCards);
+  console.log(userCards);
+
+  if (userCards.length > 0) {
+    const userCardsComponent = [];
+    for (const userCard of userCards) {
+      userCardsComponent.push(
+        <UserCard
+          alt={userCard.username}
+          distance="4km"
+          image={userCard.profilePictureUrl}
+          name={userCard.username}
+        />
+      );
+    }
+    return <div>{userCardsComponent}</div>;
+  }
 }
