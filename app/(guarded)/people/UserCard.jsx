66bc86aac9b6c1723c016a0c59bb467369409862
larsyaeger <larsyaeger@gmail.com>
2023-02-13 19:31:54 -0600

    added semi-temporary props

--- app/(guarded)/people/UserCard.jsx
+++ app/(guarded)/people/UserCard.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Button from 'app/(button)/Button';
 import { MESSAGES } from 'public/constants/pathNames';
 
-export default function UserCard() {
+export default function UserCard(props) {
   return (
     <div className="flex max-w-[250px] w-[20%] min-w-[12.5rem] m-5 flex-col justify-evenly bg-primary p-2 h-[95%] max-h-[25rem] rounded-2xl ">
       <div className="sticky aspect-square max-w-full w-auto min-h-[20%] h-[50%] self-center my-3">
@@ -14,10 +14,10 @@ export default function UserCard() {
         />
       </div>
       <h2 className="text-[1.8vh] text-center font-bold cursor-default">
-        Elon's Musk
+        {props.name || 'elons musk'}
       </h2>
       <h2 className="text-[1.8vh] text-center text-secondary cursor-default">
-        5.6km away
+        {props.distance || '5.6km away'}
       </h2>
       <div className="flex justify-center text-[1.8vh]">
         <Button addedclasses="text-sm" content="Send DM" path={MESSAGES} />
