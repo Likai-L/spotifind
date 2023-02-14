f85d9e57a4dd0f9ea37fa871919093158595ad37
larsyaeger <larsyaeger@gmail.com>
2023-02-13 18:49:56 -0600

    takes in a name and returns that user object in the response

--- src/pages/api/userSearch.js
+++ src/pages/api/userSearch.js
@@ -2,10 +2,11 @@ import prisma from '@/helpers/prisma';
 
 export default async function userSearchHandler(req, res) {
   const user = req.body;
-  console.log('user!!!!:::::', user);
-  await prisma.user({
-    where: { name: user }
+  // console.log('req.body inside userSearchHandler!', user);
+  const result = await prisma.user.findUnique({
+    where: user
   });
-  console.log('returned user from USERSEARCH:', res);
+  // console.log('returned user from userSearchHandler:', result);
+  res.send(result);
   res.end();
 }
