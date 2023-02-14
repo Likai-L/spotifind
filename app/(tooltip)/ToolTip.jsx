da40c5c9fcbcbc80f1a7a91dce7efdf7e888f4cd
Justin <justinjdbennett@gmail.com>
2023-02-13 19:45:28 -0700

    Implement tooltip in my taste

--- app/(tooltip)/ToolTip.jsx
+++ app/(tooltip)/ToolTip.jsx
@@ -1,10 +1,12 @@
 export default function ToolTip({ children, tooltip }) {
   return (
-    <div className="flex shrink-0 group relative inline-block">
+    <div className="flex shrink-0 group relative inline-block z-10">
       {children}
-      <span className="invisible group-hover:visible opacity-0 group-hover:opacity-100 transition bg-blue-500 text-primary p-1 rounded-xl absolute top-full mt-2 z-10 whitespace-nowrap">
-        {tooltip}
-      </span>
+      {tooltip ? (
+        <span className="flex font-semibold invisible group-hover:visible opacity-0 group-hover:opacity-100 transition bg-secondary text-primary p-1 rounded-lg absolute top-full mt-2 whitespace-nowrap text-center marker:overflow-auto">
+          {tooltip}
+        </span>
+      ) : null}
     </div>
   );
 }
