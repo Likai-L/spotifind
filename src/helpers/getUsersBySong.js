808798f7b64af7ab04d4d65a054d75e58c443dae
larsyaeger <larsyaeger@gmail.com>
2023-02-13 22:42:49 -0600

    currently not using this function, will refactor and use in the future

--- src/helpers/getUsersBySong.js
+++ src/helpers/getUsersBySong.js
@@ -3,11 +3,12 @@ import axios from 'axios';
 
 export default function getUsersBySong(props) {
   const { uri } = props;
-
-  axios
-    .post(FINDUSERSBYSONG, {
-      spotifySongUri: uri
-    })
-    .then(res => console.log(res.data))
+  console.log('URI THAT IS POSTED :', uri);
+  const query = axios.post(FINDUSERSBYSONG, {
+    currentSongUri: uri
+  });
+  const result = query
+    .then(res => console.log('RESPONSE.DATA!', res.data))
     .catch(err => console.log('error from getUsersbySong', err));
+  return result;
 }
