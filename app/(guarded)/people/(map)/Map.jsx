97931a7a624f72c7bae7e881e717843b0ee2d6fb
larsyaeger <larsyaeger@gmail.com>
2023-02-14 15:51:29 -0600

    no longer making another call to database, this should be complete now

--- app/(guarded)/people/(map)/Map.jsx
+++ app/(guarded)/people/(map)/Map.jsx
@@ -6,50 +6,32 @@ import 'leaflet-defaulticon-compatibility';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 // Marker and popup are for the pin, TileLayer is for crediting leaflet, MapContainer is main function
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import { useGlobalContext } from 'app/(context)';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { FINDUSERSBYSONG } from 'public/constants/pathNames';
 
-function Map() {
-  const { displayTrack } = useGlobalContext();
-  const [users, setUsers] = useState('');
-  // const { uri } = track;
-  useEffect(() => {
-    axios
-      .post(FINDUSERSBYSONG, {
-        currentSongUri: displayTrack.uri
-      })
-      .then(res => setUsers(res.data))
-      .catch(err => console.log(err));
-  }, []);
-
-  // latitude: 49.868556 longitude: -97.1713925
+function Map(props) {
+  const { users } = props;
+  console.log('props from inside map component', props);
+  console.log('users.length', users.length);
 
   if (users.length > 0) {
-    // I need a center or else the map wont work, ill make this dynamic in the future, if there's more than
-    // one user being rendered on the map, zoom out. or change this to [1] or [2] if you want to play around
-    const centerLat = users[0].latitude;
-    const centerLong = users[0].longitude;
-    const center = [Number(centerLat), Number(centerLong)];
+    const center = [0, 0];
     const userMarkers = users.map(userMarker => (
       <Marker
         // turn string lat and long back into Number by calling Number()
         position={[Number(userMarker.latitude), Number(userMarker.longitude)]}>
         <Popup>
-          marker
+          {userMarker.username}
           <br />
         </Popup>
       </Marker>
     ));
 
     return (
-      <div className="min-h-[30vh] min-w-[30vw] w-2/4 h-2/3">
+      <div className="min-h-[30vh] min-w-[30vw] w-2/4 h-[80%]">
         <MapContainer
           center={center}
           className="h-full w-full mx-5"
           scrollWheelZoom
-          zoom={13}>
+          zoom={1}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
