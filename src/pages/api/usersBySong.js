e9ab1ce64b022745e5f8f524a74f4ac0bd12c14e
larsyaeger <larsyaeger@gmail.com>
2023-02-13 22:43:37 -0600

    currently just checks for if user is currently listening, not if its recently liked

--- src/pages/api/usersBySong.js
+++ src/pages/api/usersBySong.js
@@ -1,7 +1,12 @@
-import prisma from "@/helpers/prisma";
+import prisma from '@/helpers/prisma';
 
 export default async function usersBySongHandler(req, res) {
   const songUri = req.body;
 
-  const result = await prisma
+  const result = await prisma.user.findMany({
+    where: songUri
+  });
+  console.log('result from usersBySongHandler', result);
+  res.send(result);
+  res.end;
 }
