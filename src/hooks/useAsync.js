cd65cc7db709d8198141258fd8e7d117f02425f1
Likai Liu <likailiu2@gmail.com>
2023-02-15 16:03:40 +0800

    Add a helper function that keeps track of the status of a async fcuntion

---
+++ src/hooks/useAsync.js
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useState } from 'react';
+
+function useAsyncInternal(func, dependencies, initialLoading = false) {
+  const [loading, setLoading] = useState(initialLoading);
+  const [error, setError] = useState();
+  const [value, setValue] = useState();
+
+  const execute = useCallback((...params) => {
+    setLoading(true);
+    return func(...params)
+      .then(data => {
+        setValue(data);
+        setError(undefined);
+      })
+      .catch(err => {
+        setError(err);
+        setValue(undefined);
+        return Promise.reject(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, dependencies);
+  return { loading, error, value, execute };
+}
+export function useAsync(func, dependencies = []) {
+  const { execute, ...state } = useAsyncInternal(func, dependencies, true);
+  useEffect(() => {
+    execute();
+  }, [execute]);
+  return state;
+}
+
+export function useAsyncFn(func, dependencies = []) {
+  return useAsyncInternal(func, dependencies, false);
+}
