27353a8a6217a3e65bb8080ea2f73223c9094460
Likai Liu <likailiu2@gmail.com>
2023-02-16 00:03:16 +0800

    Change file name and add a helper function that calls api/comments/update

--- src/helpers/createComment.js
+++
@@ -1,6 +0,0 @@
-import { CREATECOMMENT } from 'public/constants/pathNames';
-import makeRequest from './makeRequest';
-
-export default function createComment(data) {
-  return makeRequest(CREATECOMMENT, { method: 'POST', data }); // data: {songUri, authorUri, content, parentId}
-}
