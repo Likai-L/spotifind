14b8fd1cbaa395cb7f4885c0aafc1b5a991182fc
Justin <justinjdbennett@gmail.com>
2023-02-16 01:18:53 -0700

    Update usercard alt text and drop shadow

--- app/(guarded)/people/UserCard.jsx
+++ app/(guarded)/people/UserCard.jsx
@@ -5,11 +5,11 @@ import { MESSAGES } from 'public/constants/pathNames';
 export default function UserCard(props) {
   const { alt, image, name, distance, DmClickHandler } = props;
   return (
-    <div className="flex max-w-[250px] w-[20%] min-w-[12.5rem] m-5 flex-col justify-evenly bg-primary p-2 h-[95%] max-h-[25rem] rounded-2xl ">
+    <div className="flex max-w-[250px] w-[20%] min-w-[12.5rem] m-5 flex-col justify-evenly bg-primary p-2 h-[95%] max-h-[25rem] rounded-2xl">
       <div className="sticky aspect-square max-w-full w-auto min-h-[20%] h-[50%] self-center my-3">
         <Image
-          alt={alt || 'cute cat'}
-          className="rounded-3xl block m-auto aspect-square"
+          alt={alt || 'user avatar'}
+          className="rounded-3xl block m-auto aspect-square drop-shadow-[0_0.2rem_0.3rem_rgb(0,0,0,0.6)]"
           fill
           src={
             image ||
@@ -18,7 +18,7 @@ export default function UserCard(props) {
         />
       </div>
       <h2 className="text-[1.8vh] text-center font-bold cursor-default">
-        {name || 'elons musk'}
+        {name || 'users name'}
       </h2>
       <h2 className="text-[1.8vh] text-center text-secondary cursor-default">
         {distance || '5.6km away'}
