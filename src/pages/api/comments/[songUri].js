0a6d21639ca4eec60713e6eec29b122a52760279
Likai Liu <likailiu2@gmail.com>
2023-02-16 17:53:28 +0800

    Add like count and likedByMe fields in the comment object

--- src/pages/api/comments/[songUri].js
+++ src/pages/api/comments/[songUri].js
@@ -3,13 +3,28 @@ import prisma from '@/helpers/prisma';
 
 export default async function songCommentsHandler(req, res) {
   const { songUri } = req.query;
+  const { userUri } = req.body;
   console.log('Inside songComments api, got song Uri:', songUri);
   const comments = await prisma.comment.findMany({
     orderBy: {
       createdAt: 'desc'
     },
     where: { songUri },
-    select: COMMENT_SELECT_FIELD
+    select: { ...COMMENT_SELECT_FIELD, _count: { select: { likes: true } } }
   });
-  res.send(comments);
+
+  const likes = await prisma.like.findMany({
+    where: {
+      userUri,
+      commentId: { in: comments.map(comment => comment.id) }
+    }
+  });
+
+  const commentsWithLikes = comments.map(comment => {
+    return {
+      ...comment,
+      likedByMe: likes.find(like => like.commentId === comment.id)
+    };
+  });
+  res.send(commentsWithLikes);
 }
