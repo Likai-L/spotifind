8653cfa36dd10e5b1cc60817e6ae05952dcddcf8
Justin <justinjdbennett@gmail.com>
2023-02-15 22:05:25 -0700

    AlbumCover framer animation

--- app/(guarded)/now-playing/AlbumCover.jsx
+++ app/(guarded)/now-playing/AlbumCover.jsx
@@ -1,33 +1,36 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { motion as m } from 'framer-motion';
+import { AnimatePresence, motion as m } from 'framer-motion';
 
 export default function AlbumCover({ src, width, height, classNames, uri }) {
   return (
-    <m.div
-      animate={{ opacity: 1, scale: 1 }}
-      initial={{ opacity: 0, scale: 0.5 }}
-      transition={{
-        default: {
-          duration: 0.2,
-          ease: [0, 0.71, 0.2, 1.01]
-        },
-        scale: {
-          type: 'spring',
-          damping: 5,
-          stiffness: 100,
-          restDelta: 0.001
-        }
-      }}>
-      <Link className="flex shrink-0" href={`/songs/${uri}`}>
-        <Image
-          alt="album-cover"
-          className={`rounded-3xl cursor-pointer transition duration-200 ease-out hover:scale-105 ${classNames}`}
-          height={height}
-          src={src}
-          width={width}
-        />
-      </Link>
-    </m.div>
+    <AnimatePresence mode="wait">
+      <m.div
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0 }}
+        initial={{ opacity: 0, scale: 0.5 }}
+        transition={{
+          default: {
+            duration: 0.1,
+            ease: [0, 0.71, 0.2, 1.01]
+          },
+          scale: {
+            type: 'spring',
+            damping: 5,
+            stiffness: 100,
+            restDelta: 0.001
+          }
+        }}>
+        <Link className="flex shrink-0" href={`/songs/${uri}`}>
+          <Image
+            alt="album-cover"
+            className={`rounded-3xl cursor-pointer transition duration-200 ease-out hover:scale-105 drop-shadow-[0_0.2rem_0.3rem_rgb(0,0,0,0.6)] ${classNames}`}
+            height={height}
+            src={src}
+            width={width}
+          />
+        </Link>
+      </m.div>
+    </AnimatePresence>
   );
 }
