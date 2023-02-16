4448a6552ff69e5dacb688d1c8e468c14010c6a7
Likai Liu <likailiu2@gmail.com>
2023-02-16 11:11:32 +0800

    Send err back instead of a string

--- src/pages/api/comments/update.js
+++ src/pages/api/comments/update.js
@@ -1,7 +1,7 @@
 import prisma from '@/helpers/prisma';
 
-export default async function createCommentHandler(req, res) {
-  console.log('inside create comments api', req.body);
+export default async function updateCommentHandler(req, res) {
+  console.log('inside update comments api', req.body);
   const { content, commentId } = req.body;
   if (!content) {
     res.status(400).send({ message: 'Comment cannot be blank' });
@@ -16,6 +16,6 @@ export default async function createCommentHandler(req, res) {
     });
     res.status(200).json(comment);
   } catch (err) {
-    res.status(500).send('Database server error: please try again later');
+    res.status(500).send(err);
   }
 }
