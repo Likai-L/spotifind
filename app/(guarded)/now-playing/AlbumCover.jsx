35662ce81baf31637d3d837e3e8dd8e9d6878936
Justin <justinjdbennett@gmail.com>
2023-02-15 19:19:11 -0700

    Framer animation on albumcovers and grid

--- app/(guarded)/now-playing/AlbumCover.jsx
+++ app/(guarded)/now-playing/AlbumCover.jsx
@@ -1,16 +1,33 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { motion as m } from 'framer-motion';
 
 export default function AlbumCover({ src, width, height, classNames, uri }) {
   return (
-    <Link className="flex shrink-0" href={`/songs/${uri}`}>
-      <Image
-        alt="album-cover"
-        className={`rounded-3xl cursor-pointer transition duration-200 ease-out hover:scale-105 ${classNames}`}
-        height={height}
-        src={src}
-        width={width}
-      />
-    </Link>
+    <m.div
+      animate={{ opacity: 1, scale: 1 }}
+      initial={{ opacity: 0, scale: 0.5 }}
+      transition={{
+        default: {
+          duration: 0.2,
+          ease: [0, 0.71, 0.2, 1.01]
+        },
+        scale: {
+          type: 'spring',
+          damping: 5,
+          stiffness: 100,
+          restDelta: 0.001
+        }
+      }}>
+      <Link className="flex shrink-0" href={`/songs/${uri}`}>
+        <Image
+          alt="album-cover"
+          className={`rounded-3xl cursor-pointer transition duration-200 ease-out hover:scale-105 ${classNames}`}
+          height={height}
+          src={src}
+          width={width}
+        />
+      </Link>
+    </m.div>
   );
 }
--- app/(guarded)/songs/AlbumTile.jsx
+++ app/(guarded)/songs/AlbumTile.jsx
@@ -1,3 +1,4 @@
+import { motion as m } from 'framer-motion';
 import AlbumCover from '../now-playing/AlbumCover';
 
 export default function AlbumTile({
@@ -6,12 +7,16 @@ export default function AlbumTile({
   return (
     <div className="flex flex-col items-center font-primary p-8">
       <AlbumCover height={200} src={albumCoverUrl} uri={uri} width={200} />
-      <p className="font-bold text-2xl text-primary pt-2 pb-1 text-center  line-clamp-2">
+      <m.p
+        animate={{ opacity: 1 }}
+        className="font-bold text-2xl text-primary pt-2 pb-1 text-center  line-clamp-2"
+        initial={{ opacity: 0 }}
+        transition={{ duration: 0.75, ease: 'easeOut' }}>
         {trackName}
-      </p>
-      <p className="font-semibold text-xl text-secondary pt-1 pb-2 text-center">
+      </m.p>
+      <m.p className="font-semibold text-xl text-secondary pst-1 pb-2 text-center">
         {artist}
-      </p>
+      </m.p>
     </div>
   );
 }
